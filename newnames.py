6ecde0eab9dd5717bbed91fca1372b970045a448
Shane Lin <ssl2ab@virginia.edu>
2018-09-18 22:41:04 -0400

    Identify duplicate names (vs merely collisions)

--- newnames.py
+++ newnames.py
@@ -1,6 +1,7 @@
 import json
 from io import StringIO
 import re
+import collections
 
 with open("./canon_names.json", 'r') as name_in:
         names = json.load(name_in)
@@ -8,31 +9,54 @@ name_map = {}
 badnames = []
 ambiguous = []
 nomatch = []
-collisions = {}
+nomatch2 = []
+duplicates = collections.defaultdict(list)
+collisions = collections.defaultdict(list)
 
 alphas = re.compile(r'[^\w ]+', re.UNICODE)
+
+for dupe_name in [name for name, count in collections.Counter([alphas.sub('',n.lower()) for n in names.values()]).items() if count > 1]:
+    for id in names:
+        if alphas.sub('',names[id]).lower() == dupe_name:
+            duplicates[dupe_name].append(id)
 
 for id in names:
     if re.match(r'^[a-z]{6}[^a-z|$]*', id):
         nsplit = alphas.sub('',names[id]).lower().split(" ")
-        print(nsplit)
         startswithid = list(filter(lambda x: x.startswith(id[:3]), nsplit))
+
         if len(startswithid)>1:
             ambiguous.append(id)
-            continue
+            # Note the ambiguity, but assume that the sort-name is the last split
+        
         elif len(startswithid)<1:
             nomatch.append(id)
             continue
-        nsplit.remove(startswithid[0])
-        if len(nsplit):
-            name_map[id]=startswithid[0]+"."+".".join(nsplit)
-        else:
-            name_map[id]=startswithid[0]
         
-        if name_map[id] not in collisions:
-            collisions[name_map[id]] = [id]
-        else:
-            collisions[name_map[id]].append(id)
+        new_id1 = startswithid[-1]
+        nsplit.remove(new_id1)
+        new_id = [new_id1]
+
+        if len(nsplit):
+            # Sometimes the secondary ID prefix occupies index 4-6 of the old ID rather than 3-6
+            startswithid3 = list(filter(lambda x: x.startswith(id[3:6]), nsplit))
+            startswithid4 = list(filter(lambda x: x.startswith(id[4:6]), nsplit))
+            if len(startswithid3)>0:
+                new_id.append(startswithid3[0])
+                nsplit.remove(startswithid3[0])
+                new_id.extend(nsplit)
+            elif len(startswithid4)>0:
+                new_id.append(startswithid4[0])
+                nsplit.remove(startswithid4[0])
+                new_id.extend(nsplit)
+            else:
+                nomatch2.append(id)
+                continue
+
+        name_map[id]=".".join(new_id)
+        
+        collisions[name_map[id]].append(id)
+
     else:
         badnames.append(id)
 
@@ -45,9 +69,17 @@ for bad in badnames:
 print("\n\nAmbiguous names:")
 for amb in ambiguous:
     print(amb+" ("+names[amb]+")")
-print("\n\nNo match names:")
+print("\n\nNo match ID1 names:")
 for nom in nomatch:
     print(nom+" ("+names[nom]+")")
+print("\n\nNo match ID2 names:")
+for nom in nomatch2:
+    print(nom+" ("+names[nom]+")")
+print("\n\nDuplicate names:")
+for dup in duplicates:
+    print(dup+":")
+    for d in duplicates[dup]:
+        print("\t"+d+" ("+names[d]+")")
 print("\n\nCollision names:")
 collision_ids = list(filter(lambda x: len(collisions[x])>1, collisions))
 for col in collision_ids:
